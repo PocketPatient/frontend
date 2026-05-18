fc5bbdb632c1460d66e75ddbcbd230db4e7e534c
Mahir Shah <mahir.shah@rutgers.edu>
2026-05-17 21:50:26 -0400

    feat: initialize Firebase and request notification permissions

--- pocket_patient/lib/main.dart
+++ pocket_patient/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.requestPermission();
   runApp(const ProviderScope(child: App()));
 }
